bfa43e3c1aaebe52171137425d2385b42e95ddca
stewjwebb <stewjwebb@gmail.com>
2010-02-17 08:13:50 -0800

    update hello with best app text

--- hello.rb
+++ hello.rb
@@ -1,1 +1,2 @@
 puts 'shalomecho puts holaecho "puts holaecho puts 'holaecho puts holagit commit -m "changed greeting from English to French" > hello.rb  && git status && git commit -am changed greeting from French to Spanish'
+Welcome to best Ruby 
