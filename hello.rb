69b0ef3f59bec273ff6a2409a3f1623db32cb981
stewjwebb <stewjwebb@gmail.com>
2010-02-17 08:32:05 -0800

    extra

--- hello.rb
+++ hello.rb
@@ -1,2 +1,3 @@
 puts 'shalomecho puts holaecho "puts holaecho puts 'holaecho puts holagit commit -m "changed greeting from English to French" > hello.rb  && git status && git commit -am changed greeting from French to Spanish'
+my extra stuff
 Welcome to best Ruby 
