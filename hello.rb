0d4a75a675d7b0ed896910d9e26883be560efbdc
Robot Made <github@robotarmyma.de>
2010-02-23 20:14:47 -0800

    changed greeting from Spanish to Hebrew

--- hello.rb
+++ hello.rb
@@ -1,1 +1,1 @@
-puts 'holaecho puts holaecho "puts holaecho puts 'holagit commit -m changed greeting from English to French'
+puts 'shalomecho puts holaecho "puts holaecho puts 'holaecho puts holagit commit -m "changed greeting from English to French" > hello.rb  && git status && git commit -am changed greeting from French to Spanish'
